22108078d62b5aa1526f68516f1947733b10f8b2
Nicolas Cisco <n.cis_92@hotmail.com>
2015-04-28 01:43:25 -0300

    Ultimo arrelgo para que ande bien esto?

--- build_tools/build_current_tag.sh
+++ build_tools/build_current_tag.sh
@@ -12,7 +12,7 @@ ROCKSDB_MAJOR=`egrep "ROCKSDB_MAJOR.[0-9]" include/rocksdb/version.h | cut -d ' 
 ROCKSDB_MINOR=`egrep "ROCKSDB_MINOR.[0-9]" include/rocksdb/version.h | cut -d ' ' -f 3`
 ROCKSDB_PATCH=`egrep "ROCKSDB_PATCH.[0-9]" include/rocksdb/version.h | cut -d ' ' -f 3`
 #pkgver=`git symbolic-ref HEAD | sed -e "s/^refs\/heads\/v//"`-1
-pkgver="$ROCKSDB_MAJOR\.$ROCKSDB_MINOR-$ROCKSDB_PATCH"
+pkgver="$ROCKSDB_MAJOR.$ROCKSDB_MINOR-$ROCKSDB_PATCH"
 #pkgdir="rocksdb_$pkgver-1"
 pkgdir="rocksdb_$pkgver"
 
@@ -21,7 +21,7 @@ mkdir "$pkgdir/usr"
 mkdir "$pkgdir/usr/lib"
 install -d "$pkgdir/usr/include"
 cp -r include/rocksdb "$pkgdir"/usr/include
-install -m755 -D librocksdb*.so "$pkgdir"/usr/lib/librocksdb.so
+install -m755 -D librocksdb.so "$pkgdir"/usr/lib/librocksdb.so
 
 mkdir "$pkgdir/DEBIAN"
 echo "Package: RocksDb" >> "$pkgdir/DEBIAN/control"
@@ -31,10 +31,10 @@ echo "Priority: optional" >> "$pkgdir/DEBIAN/control"
 echo "Architecture: amd64" >> "$pkgdir/DEBIAN/control"
 #echo "Depends: libsomethingorrather (>= 1.2.13), anotherDependency (>= 1.2.6)" >> "$pkgdir/DEBIAN/control"
 echo "Maintainer: Your Name <you@email.com>" >> "$pkgdir/DEBIAN/control"
-echo "Description: Hello World" >> "$pkgdir/DEBIAN/control"
-echo " When you need some sunshine, just run this" >> "$pkgdir/DEBIAN/control"
-echo " small program!" >> "$pkgdir/DEBIAN/control"
+echo "Description: Rocksdb db package for travis ci" >> "$pkgdir/DEBIAN/control"
+echo " This package was done in order to avoid compiling rocksdb every time" >> "$pkgdir/DEBIAN/control"
+echo " while developing a proyect using travis ci!" >> "$pkgdir/DEBIAN/control"
 echo " " >> "$pkgdir/DEBIAN/control"
-echo " (the space before each line in the description is important)" >> "$pkgdir/DEBIAN/control"
+echo " :)" >> "$pkgdir/DEBIAN/control"
 
 dpkg-deb --build "$pkgdir"
